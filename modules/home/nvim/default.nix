9f409e2ff990e900b851d708cfa37647b2b2925b
HexReloc <hexreloc@gmail.com>
2026-09-02 08:38:02 +0545

    home-nvim root

---
+++ modules/home/nvim/default.nix
@@ -0,0 +1,101 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    withRuby = false;
+    withPython3 = false;
+
+    extraPackages = with pkgs; [
+      lua-language-server
+      nixd
+      clang-tools
+
+      xclip
+      wl-clipboard
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+
+      # LSP
+      {
+        plugin = nvim-lspconfig;
+        type = "lua";
+        config = builtins.readFile ./plugin/lsp.lua;
+      }
+
+      # Comments
+      {
+        plugin = comment-nvim;
+        type = "lua";
+        config = ''require("Comment").setup()'';
+      }
+
+      # Theme
+      {
+        plugin = gruvbox-nvim;
+        type = "viml";
+        config = "colorscheme gruvbox";
+      }
+
+      # Lua development
+      neodev-nvim
+
+      # Completion
+      {
+        plugin = nvim-cmp;
+        type = "lua";
+        config = builtins.readFile ./plugin/cmp.lua;
+      }
+
+      cmp_luasnip
+      cmp-nvim-lsp
+      luasnip
+      friendly-snippets
+
+      # Telescope
+      {
+        plugin = telescope-nvim;
+        type = "lua";
+        config = builtins.readFile ./plugin/telescope.lua;
+      }
+
+      telescope-fzf-native-nvim
+
+      # Statusline / icons
+      lualine-nvim
+      nvim-web-devicons
+
+      # Treesitter
+      {
+        plugin = (
+          nvim-treesitter.withPlugins (p: [
+            p.tree-sitter-nix
+            p.tree-sitter-vim
+            p.tree-sitter-bash
+            p.tree-sitter-lua
+            p.tree-sitter-python
+            p.tree-sitter-json
+            p.tree-sitter-c
+            p.tree-sitter-cpp
+          ])
+        );
+        type = "lua";
+        config = builtins.readFile ./plugin/treesitter.lua;
+      }
+
+      # Nix filetype support
+      vim-nix
+    ];
+
+    initLua = ''
+      ${builtins.readFile ./options.lua}
+    '';
+  };
+}
+
