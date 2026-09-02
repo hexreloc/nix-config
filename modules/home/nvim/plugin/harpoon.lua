39bb4a54fa33c338437e5db14e8ef04746f5015c
HexReloc <hexreloc@gmail.com>
2026-09-02 09:48:48 +0545

    nvim: added harpoon

--- modules/home/nvim/plugin/harpoon.lua
+++ modules/home/nvim/plugin/harpoon.lua
@@ -2,17 +2,18 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
+vim.keymap.del("n", "<leader>a")
+
 vim.keymap.set("n", "<leader>a", function()
-  harpoon:list():add()
+	harpoon:list():add()
 end, { desc = "Harpoon: Add file" })
 
 vim.keymap.set("n", "<C-e>", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
+	harpoon.ui:toggle_quick_menu(harpoon:list())
 end, { desc = "Harpoon: Toggle menu" })
 
 for i = 1, 4 do
-  vim.keymap.set("n", "<leader>" .. i, function()
-    harpoon:list():select(i)
-  end, { desc = "Harpoon: File " .. i })
+	vim.keymap.set("n", "<leader>" .. i, function()
+		harpoon:list():select(i)
+	end, { desc = "Harpoon: File " .. i })
 end
-
