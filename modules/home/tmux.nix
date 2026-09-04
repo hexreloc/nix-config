4f22059312d3c616236c6954167520885a0b0c0b
HexReloc <hexreloc@gmail.com>
2026-09-04 20:13:42 +0545

    (home) added tmux

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -4,40 +4,21 @@
     mouse = true;
     shortcut = "a";
     historyLimit = 5000;
+
     extraConfig = ''
-      set -g visual-activity off
-  set -g visual-bell off
-  set -g visual-silence off
-  setw -g monitor-activity off
-  set -g bell-action none
+      set -g default-terminal "tmux-256color"
 
-  setw -g clock-mode-colour yellow
+      set -g status-style "bg=default,fg=default"
+      set -g status-left " #S "
+      set -g status-right " %H:%M "
 
-  setw -g mode-style 'fg=black bg=red bold'
+      setw -g window-status-current-format " #I:#W "
 
-  set -g pane-border-style 'fg=red'
-  set -g pane-active-border-style 'fg=yellow'
+      setw -g window-status-current-format " #I:#W "
 
-  set -g status-position bottom
-  set -g status-justify left
-  set -g status-style 'fg=red'
-
-  set -g status-left-length 10
-
-  set -g status-right-style 'fg=black bg=yellow'
-  set -g status-right '%Y-%m-%d %H:%M '
-  set -g status-right-length 50
-
-  setw -g window-status-current-style 'fg=black bg=red'
-  setw -g window-status-current-format ' #I #W #F '
-
-  setw -g window-status-style 'fg=red bg=black'
-  setw -g window-status-format ' #I #[fg=white]#W #[fg=yellow]#F '
-
-  setw -g window-status-bell-style 'fg=yellow bg=red bold'
-
-  # messages
-  set -g message-style 'fg=yellow bg=red bold'
+      set -g pane-border-style "fg=brightblack"
+      set -g pane-active-border-style "fg=white"
     '';
   };
 }
+
