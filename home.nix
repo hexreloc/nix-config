ff5dd388a405d2cbb5a891ee18cfc3f1133d9920
HexReloc <hexreloc@gmail.com>
2026-09-04 20:09:46 +0545

    (home) added i3status and ns

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{config, pkgs, ...}:
+{pkgs, ...}:
 
 {
 	imports = [
@@ -9,10 +9,24 @@
 	home.packages = with pkgs;[
 		firefox #browser
 		pavucontrol # volume control
-        i3status #statusbar
+        i3status-rust #statusbar rs impl
         feh # wallpaper
         zathura # pdf viewier
         digital # cicuit simulator
-	];
+        discord
+        obsidian
+        ripgrep
+
+        (pkgs.writeShellApplication {
+          name = "ns";
+          runtimeInputs = with pkgs; [
+            fzf
+            nix-search-tv
+          ];
+          text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
+        })
+
+      ];
+      
 	home.stateVersion = "25.11";
 }
