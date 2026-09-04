baf84634689291c313a0afb60371347d4f962d35
HexReloc <hexreloc@gmail.com>
2026-09-04 20:10:43 +0545

    (home) added aliases

--- modules/home/bash.nix
+++ modules/home/bash.nix
@@ -2,8 +2,9 @@
 	programs.bash = {
 		enable = true;
 		shellAliases = {
-			btw = "echo i use nixos btw";
-			nrs = "sudo nixos-rebuild switch --flake ~/nix-config#nixos-btw";
+          btw = "echo i use nixos btw";
+          ff = "fastfetch";
+		  nrs = "sudo nixos-rebuild switch --flake ~/nix-config#nixos-btw";
 		};
 		initExtra = ''
 		export PS1='\[\e[38;5;149m\]\u\[\e[0m\] in \[\e[38;5;39m\]\w\[\e[0m\] \\$ '
